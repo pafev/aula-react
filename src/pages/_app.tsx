8226df16bc38ccd1f86a89a7aca2607aafe586e5
paulo <paulofvilarimda@gmail.com>
2023-11-23 14:51:21 -0300

    font cairo play

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import Navbar from '@/components/navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Cairo_Play } from 'next/font/google'
+
+const font = Cairo_Play({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
+  <div className={font.className}>
     <Navbar/>
     <Component {...pageProps} />
-  </>)
+  </div>)
 }
